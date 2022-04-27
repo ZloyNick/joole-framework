666fa648402f7b9e24279c9a7a376088c1d20cb7
ZloyNick <sitdikov_r@magnit.ru>
2022-04-27 15:07:27 +0500

    Release v8.1.3.1
    * Router deleted and moved to https://github.com/ZloyNick/joole-components-routing
    * RequestValidator::response() implementation

--- config/joole.php
+++ config/joole.php
@@ -4,12 +4,5 @@
  * Configuration file for Joole class.
  */
 return [
-    'components' => [
-        [
-            'name' => 'router',
-            'class' => \joole\framework\routing\BaseRouter::class,
-            'options' => [],
-            'routes' => __DIR__ . '/routes/',
-        ],
-    ],
+    'components' => [],
 ];
